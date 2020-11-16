031e7ce5f86913c1987d5310aff78d536eced0c2
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2020-11-16 11:07:05 -0800

    read to receive todos

--- app/src/Volunteer.js
+++ app/src/Volunteer.js
@@ -1,10 +1,62 @@
 import React from 'react'
+import styled from 'styled-components'; 
+
+
+const StyledVolunteer = styled.div`
+
+
+
+.title {
+    margin: 1%; 
+    font-size: 10rem;
+}
+
+.todo-list {
+    display: flex; 
+    flex-direction: column;
+    margin: 1%; 
+    width: 50%; 
+}
+
+.todo {
+    display: flex; 
+    justify-content: space-around; 
+    align-items: center;
+    width: 30%; 
+    margin: 2%;
+    font-size: 2.5rem; 
+}
+
+
+h2 {
+    width: 80%; 
+}
+
+input {
+    width: 20%; 
+}
+.active {
+    background: red; 
+    text-decoration: line-through; 
+}
+
+`
 
 const Volunteer = () => {
+
+  
+
     return (
-        <div>
-            Test
-        </div>
+        <StyledVolunteer>
+            <div className="title">
+                <h1>To-Do List</h1>
+            </div>
+            <div className="todo-list">
+                
+            </div>
+            <button >Clear Completed!</button>
+
+        </StyledVolunteer>
     )
 }
 
