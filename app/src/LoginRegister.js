80f292972cddc749baaa6dba1a4cb044286710b2
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2020-11-16 11:44:28 -0700

    created html layout for forms

--- app/src/LoginRegister.js
+++ app/src/LoginRegister.js
@@ -3,13 +3,126 @@ import './LoginRegister.css';
 
 export default function LoginRegister() {
     return (
-        <form>
-            <div>
-                <input type="text" placeholder="Username" />
-                <input type="password" placeholder="Password" />
-                <button type='submit'>Login</button>
-            </div>
-        </form>
+        <div>
+            <form id="loginForm">
+                <div>
+                    <div>{errors.username}</div>
+                    <div>{errors.password}</div>
+                </div>
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <button type='submit'>Login</button>
+                    <label><span>Register</span></label>
+                </div>
+            </form>
+            <form id='registerForm'>
+                <div>
+                    <div>{errors.username}</div>
+                    <div>{errors.email}</div>
+                    <div>{errors.password}</div>
+                    <div>{errors.confirmPassword}</div>
+                    <div>{errors.role}</div>
+                    <div>{errors.availability}</div>
+                    <div>{errors.country}</div>
+                </div>
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                    />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                    />
+                    <input
+                        type="password"
+                        placeholder="Enter Password"
+                    />
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                    />
+                    <div>
+                        <label>
+                            Role
+                        <select>
+                                <option value="">- Select an option -</option>
+                                <option value="admin">Admin</option>
+                                <option value="student">Student</option>
+                                <option value="volunteer">Volunteer</option>
+                            </select>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Availability
+                             <div>
+                                <label>
+                                    Monday
+                                 <input
+                                        type="checkbox"
+                                    />
+                                </label>
+                                <label>
+                                    Tuesday
+                                 <input
+                                        type="checkbox"
+                                    />
+                                </label>
+                                <label>
+                                    Wednesday
+                                 <input
+                                        type="checkbox"
+                                    />
+                                </label>
+                                <label>
+                                    Thursday
+                                 <input
+                                        type="checkbox"
+                                    />
+                                </label>
+                                <label>
+                                    Friday
+                                 <input
+                                        type="checkbox"
+                                    />
+                                </label>
+                                <label>
+                                    Saturday
+                                 <input
+                                        type="checkbox"
+                                    />
+                                </label>
+                                <label>
+                                    Sunday
+                                 <input
+                                        type="checkbox"
+                                    />
+                                </label>
+                            </div>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Country
+                             <input
+                                type="text"
+                                placeholder="Enter Country"
+                            />
+                        </label>
+                    </div>
+                    <button type='submit'>Register</button>
+                    <label for='form-switch'>Already Member ? Sign In Now..</label>
+                </div>
+            </form>
+        </div>
     )
 }
 
